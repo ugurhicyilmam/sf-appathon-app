3182b9510da00e3500c445315758d1d31af030ba
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2017-10-25 23:59:31 +0300

    add home-owner parking lot status

--- src/app/home-owner/home-owner.component.ts
+++ src/app/home-owner/home-owner.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
+import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
 
 @Component({
   selector: 'app-home-owner',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeOwnerComponent implements OnInit {
 
-  constructor() { }
+  parkingLotStatus: any;
+
+  constructor(private http: Http) {
+    this.parkingLotStatus = 0;
+  }
+
+
+  getStatus(tags) {
+    let status = 0;
+    try {
+      status = tags[0]['results'][0]['values'][0][1];
+    } catch (e) {
+    }
+    return status;
+  }
 
   ngOnInit() {
+    const self = this;
+    IntervalObservable.create(1000).subscribe(function () {
+      const requestBody = {
+        'start': '1h-ago',
+        'tags': [
+          {
+            'name': 'WR-IDP-F24E:button',
+            'order': 'desc',
+            'limit': 1
+          }
+        ]
+      };
+      self.http.post('https://mmsanfranciscoteam-web-api.run.aws-usw02-pr.ice.predix.io/api/TimeSeries', requestBody).subscribe((response) => {
+        self.parkingLotStatus = self.getStatus(response.json().tags);
+      });
+  });
+
   }
 
 }
